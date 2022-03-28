70eaf37481286fa18bf2ba430dcc882e94870731
Vansh Wadhwa <vanshwadhwasphere@gmail.com>
2022-03-28 13:17:48 +0530

    Added heatmap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Heatmap from "./pages/Heatmap";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/heatmap" element={<Heatmap />} />
 
           {/* <Route path="invoices" element={<Invoices />} /> */}
         </Routes>
