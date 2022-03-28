2aeab15f9ad4eec6f98f9963f0dc9202f83f83cd
Vansh Wadhwa <vanshwadhwasphere@gmail.com>
2022-03-29 00:42:51 +0530

    added charts

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,19 @@
 import React from "react";
 import DashboardTable from "../components/DashboardTable";
+import PieChart from "../components/PieChart";
+import Heatmap from "./Heatmap";
 
 const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
       <DashboardTable />
+      <div class="max-w-lg  m-4">
+        <Heatmap />
+      </div>
+      <div class="max-w-sm  m-4">
+        <PieChart />
+      </div>
     </div>
   );
 };
--- src/pages/Heatmap.js
+++ src/pages/Heatmap.js
@@ -28,11 +28,28 @@ const Heatmap = (props) => {
       { lat: 28.677718522771112, lng: 77.26122447557762 },
       { lat: 28.67788158809669, lng: 77.25997174564884 },
       { lat: 28.678743875418736, lng: 77.26281848813903 },
-
       { lat: 28.680277544245573, lng: 77.26069274000719 },
+
+      { lat: 28.4653410425, lng: 77.54656501337 },
+      { lat: 28.8465318522771112, lng: 77.5653437762 },
+      { lat: 28.5453458809669, lng: 77.7848664884 },
+      { lat: 28.5435643875418736, lng: 77.1231348813903 },
+      { lat: 28.54185544245573, lng: 77.54314000719 },
+
+      { lat: 27.435410425, lng: 75.5413233 },
+      { lat: 27.5435318522, lng: 75.5343437762 },
+      { lat: 27.4354458809, lng: 75.65432664884 },
+      { lat: 27.9868643875, lng: 75.324148814 },
+      { lat: 27.5313554424, lng: 75.5313200719 },
+
+      { lat: 29.5454543541, lng: 76.54321321 },
+      { lat: 29.1221321311, lng: 76.43413512 },
+      { lat: 29.5465564642, lng: 76.54211212 },
+      { lat: 29.5451312214, lng: 76.12489741 },
+      { lat: 29.4354165676, lng: 76.65321323 },
     ],
     options: {
-      radius: 20,
+      radius: 10,
       opacity: 0.6,
     },
   };
@@ -52,7 +69,7 @@ const Heatmap = (props) => {
 
   return (
     // Important! Always set the container height explicitly
-    <div style={{ height: "100vh", width: "100%" }}>
+    <div style={{ height: "50vh", width: "100%" }}>
       <GoogleMapReact
         // ref={(el) => (this._googleMap = el)}
         bootstrapURLKeys={"AIzaSyAY3hshDA-xeK7ME1UY3HDBdNAAv8UIO3k"}
